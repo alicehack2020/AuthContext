d24488d9f6e878d9d702fbf115cd5ea5bdbc87dd
Mangesh2042 <mangesh2042@gmail.com>
2022-07-17 12:08:09 +0530

    login logout added

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React,{useContext} from 'react'
+import {AuthContext} from "../context/AuthContext"
+
+const HomePage = () => {
+const {logout}=useContext(AuthContext)
+  return (
+    <div onClick={()=>logout()}>
+     logout
+    </div>
+  )
+}
+
+export default HomePage
